3676f5ad15af1277340e5bddc90ce43b9b64b35f
horumiko <79800115+horumiko@users.noreply.github.com>
2023-03-12 15:51:38 +0300

    fix tasks

--- js/task_module.js
+++ js/task_module.js
@@ -82,17 +82,6 @@ const tasks = [
     comments: []
   },
   {
-    id: '1',
-    name: 'Create ios app',
-    description: 'swift',
-    createdAt: new Date('2023-03-09T13:20:00'),
-    assignee: 'Alena',
-    status: 'To Do',
-    priority: 'High',
-    isPrivate: true,
-    comments: []
-  },
-  {
     id: '8',
     name: 'Create tg chat',
     description: 'py + telegramapi',
